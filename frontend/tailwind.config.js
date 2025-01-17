db8b1d039741ff503dbc1d553dc43fac09eeb35b
sultechpro <apatirasulayman@gmail.com>
2025-01-17 10:14:44 +0100

    Set up Vite development server with initial React and routing configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./public/**/*.html",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
